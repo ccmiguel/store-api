2d0716b18d577f74aa991b37506f3e5f3249962f
Miguel Calderon <105742725+ccmiguel@users.noreply.github.com>
2024-06-05 17:20:27 +0000

    Avance 05/06/2024

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,297 +1,38 @@
-{
-    "openapi": "3.0.1",
-    "info": {
-        "title": "Tienda Online API",
-        "version": "1.0.0"
-    },
-    "paths": {
-        "/api/products": {
-            "get": {
-                "summary": "Obtiene la lista de todos los productos",
-                "security": [
-                    {
-                        "JWTAuth": []
-                    }
-                ],
-                "responses": {
-                    "200": {
-                        "description": "Lista de productos",
-                        "content": {
-                            "application/json": {
-                                "schema": {
-                                    "type": "array",
-                                    "items": {
-                                        "$ref": "#/components/schemas/Product"
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            },
-            "post": {
-                "summary": "Crea un nuevo producto",
-                "security": [
-                    {
-                        "JWTAuth": []
-                    }
-                ],
-                "requestBody": {
-                    "content": {
-                        "application/json": {
-                            "schema": {
-                                "$ref": "#/components/schemas/Product"
-                            }
-                        }
-                    }
-                },
-                "responses": {
-                    "201": {
-                        "description": "Producto creado",
-                        "content": {
-                            "application/json": {
-                                "schema": {
-                                    "$ref": "#/components/schemas/Product"
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        },
-        "/api/products/{id}": {
-            "get": {
-                "summary": "Obtiene un producto específico por su ID",
-                "security": [
-                    {
-                        "JWTAuth": []
-                    }
-                ],
-                "parameters": [
-                    {
-                        "name": "id",
-                        "in": "path",
-                        "required": true,
-                        "schema": {
-                            "type": "integer"
-                        }
-                    }
-                ],
-                "responses": {
-                    "200": {
-                        "description": "Detalles del producto",
-                        "content": {
-                            "application/json": {
-                                "schema": {
-                                    "$ref": "#/components/schemas/Product"
-                                }
-                            }
-                        }
-                    },
-                    "404": {
-                        "description": "Producto no encontrado"
-                    }
-                }
-            },
-            "put": {
-                "summary": "Actualiza un producto existente por su ID",
-                "security": [
-                    {
-                        "JWTAuth": []
-                    }
-                ],
-                "parameters": [
-                    {
-                        "name": "id",
-                        "in": "path",
-                        "required": true,
-                        "schema": {
-                            "type": "integer"
-                        }
-                    }
-                ],
-                "requestBody": {
-                    "content": {
-                        "application/json": {
-                            "schema": {
-                                "$ref": "#/components/schemas/Product"
-                            }
-                        }
-                    }
-                },
-                "responses": {
-                    "200": {
-                        "description": "Producto actualizado",
-                        "content": {
-                            "application/json": {
-                                "schema": {
-                                    "$ref": "#/components/schemas/Product"
-                                }
-                            }
-                        }
-                    },
-                    "404": {
-                        "description": "Producto no encontrado"
-                    }
-                }
-            },
-            "delete": {
-                "summary": "Elimina un producto existente por su ID",
-                "security": [
-                    {
-                        "JWTAuth": []
-                    }
-                ],
-                "parameters": [
-                    {
-                        "name": "id",
-                        "in": "path",
-                        "required": true,
-                        "schema": {
-                            "type": "integer"
-                        }
-                    }
-                ],
-                "responses": {
-                    "204": {
-                        "description": "Producto eliminado"
-                    },
-                    "404": {
-                        "description": "Producto no encontrado"
-                    }
-                }
-            }
-        },
-        "/api/register": {
-            "post": {
-                "summary": "Registra un nuevo usuario",
-                "requestBody": {
-                    "content": {
-                        "application/json": {
-                            "schema": {
-                                "$ref": "#/components/schemas/User"
-                            }
-                        }
-                    }
-                },
-                "responses": {
-                    "201": {
-                        "description": "Usuario creado"
-                    },
-                    "400": {
-                        "description": "Solicitud incorrecta"
-                    }
-                }
-            }
-        },
-        "/api/login": {
-            "post": {
-                "summary": "Inicia sesión con un usuario existente",
-                "requestBody": {
-                    "content": {
-                        "application/json": {
-                            "schema": {
-                                "$ref": "#/components/schemas/Login"
-                            }
-                        }
-                    }
-                },
-                "responses": {
-                    "200": {
-                        "description": "Inicio de sesión exitoso",
-                        "content": {
-                            "application/json": {
-                                "schema": {
-                                    "$ref": "#/components/schemas/Token"
-                                }
-                            }
-                        }
-                    },
-                    "401": {
-                        "description": "Credenciales inválidas"
-                    }
-                }
-            }
-        }
-    },
-    "components": {
-        "securitySchemes": {
-            "JWTAuth": {
-                "type": "http",
-                "scheme": "bearer",
-                "bearerFormat": "JWT"
-            }
-        },
-        "schemas": {
-            "Product": {
-                "type": "object",
-                "properties": {
-                    "id": {
-                        "type": "integer",
-                        "readOnly": true
-                    },
-                    "name": {
-                        "type": "string"
-                    },
-                    "description": {
-                        "type": "string"
-                    },
-                    "price": {
-                        "type": "number",
-                        "format": "float"
-                    },
-                    "stock": {
-                        "type": "integer"
-                    }
-                },
-                "required": [
-                    "name",
-                    "description",
-                    "price",
-                    "stock"
-                ]
-            },
-            "User": {
-                "type": "object",
-                "properties": {
-                    "username": {
-                        "type": "string"
-                    },
-                    "password": {
-                        "type": "string"
-                    },
-                    "roles": {
-                        "type": "array",
-                        "items": {
-                            "type": "string",
-                            "enum": [
-                                "admin",
-                                "user"
-                            ]
-                        }
-                    }
-                },
-                "required": [
-                    "username",
-                    "password",
-                    "roles"
-                ]
-            },
-            "Login": {
-                "type": "object",
-                "properties": {
-                    "username": {
-                        "type": "string"
-                    },
-                    "password": {
-                        "type": "string"
-                    }
-                },
-                "required": [
-                    "username",
-                    "password"
-                ]
-            }
-        }
-    }
-}
+import pytest
+from flask_jwt_extended import create_access_token
+
+from app.database import db
+from app.run import app
+
+
+@pytest.fixture(scope="module")
+def test_client():
+    app.config["TESTING"] = True
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
+    app.config["JWT_SECRET_KEY"] = "test_secret_key"
+
+    with app.test_client() as testing_client:
+        with app.app_context():
+            db.create_all()
+            yield testing_client
+            db.drop_all()
+
+
+@pytest.fixture(scope="module")
+def admin_auth_headers():
+    with app.app_context():
+        access_token = create_access_token(
+            identity={"username": "testuser", "roles": '["admin"]'}
+        )
+        headers = {"Authorization": f"Bearer {access_token}"}
+        return headers
+
+
+@pytest.fixture(scope="module")
+def user_auth_headers():
+    with app.app_context():
+        access_token = create_access_token(
+            identity={"username": "user", "roles": '["user"]'}
+        )
+        headers = {"Authorization": f"Bearer {access_token}"}
+        return headers
